153c807fa6ce612867f6c9bffd8bee4d17d6db6a
KavyaJP <kavya31052005@gmail.com>
2025-08-28 10:08:23 +0530

    query string and parameter handling

--- ExpressJS2/app.js
+++ ExpressJS2/app.js
@@ -21,6 +21,20 @@ app.get('/contact', (req, res) => {
   res.send('Contact page!')
 })
 
+// Parameter handling
+// http://127.0.0.1:3000/product/id
+app.get('/product/:id', (req, res) => {
+  var a = req.params.id
+  res.send(`Product details is ${a}`)
+})
+
+// Query string
+// http://127.0.0.1:3000/search?q=string
+app.get('/search', (req, res) => {
+  var a = req.query.q
+  res.send(`Product details is ${a}`)
+})
+
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
 })
