6465ce7274ad024e35196d23fb6982657e77dc06
KavyaJP <kavya31052005@gmail.com>
2025-10-08 12:07:49 +0530

    Practical 1

---
+++ PracticalList/practical1/src/App.jsx
@@ -0,0 +1,38 @@
+import { Routes, Route, Link } from 'react-router-dom';
+
+import Home from './Home.jsx';
+import About from './About.jsx';
+import Contact from './Contact.jsx';
+import './App.css';
+
+function App() {
+  return (
+    <div>
+      <nav>
+        <ul>
+          <li>
+            <Link to="/">Home</Link>
+          </li>
+          <li>
+            <Link to="/about">About</Link>
+          </li>
+          <li>
+            <Link to="/contact">Contact</Link>
+          </li>
+        </ul>
+      </nav>
+
+      <hr />
+
+      <main>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/about" element={<About />} />
+          <Route path="/contact" element={<Contact />} />
+        </Routes>
+      </main>
+    </div>
+  );
+}
+
+export default App;
